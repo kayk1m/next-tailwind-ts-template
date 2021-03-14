2a9d1581bccdf13c5125471c3007c4e15ee3cd78
Kay Kim <kimjh@bawi.org>
2021-03-14 20:17:56 +0900

    remove tailwind configurations
    remove global types
    
    add ui.d.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,30 +6,6 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontSize: {
-      xs: ['11px', '1rem'],
-      sm: ['13px', '1.25rem'],
-      base: ['15px', '1.5rem'],
-      lg: ['17px', '1.75rem'],
-      xl: ['22px', '1.75rem'],
-      '2xl': ['25px', '2rem'],
-      '3xl': ['32px', '2.25rem'],
-      '4xl': ['40px', '2.5rem'],
-      '5xl': ['48px', '3rem'],
-      '6xl': ['60px', '3.75rem'],
-      '7xl': ['72px', '4.5rem'],
-      '8xl': ['96px', '6rem'],
-      '9xl': ['128px', '8rem'],
-    },
-    fontFamily: {
-      sans: [
-        'Apple SD Gothic Neo',
-        'Noto Sans CJK KR',
-        '맑은 고딕',
-        'Malgun Gothic',
-        'sans-serif',
-      ],
-    },
     extend: {
       height: {
         4.5: '18px',
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,19 +1,1 @@
 // Declarations for global interfaces & types
-interface CustomError extends Error {
-  code?: number;
-  additionalInfo?: unknown;
-}
-
-interface Kakao {
-  init: (key: string) => void;
-  isInitialized: () => boolean;
-  Channel: {
-    chat: ({ channelPublicId: string }) => void;
-  };
-}
-
-interface Window {
-  Kakao: Kakao;
-}
-
-declare module 'shaka-player/dist/shaka-player.ui';
