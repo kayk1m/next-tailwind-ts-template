305658e34b8c7ca02c9e4407ab778b79d47201e6
Kay Kim <kimjh@bawi.org>
2021-04-16 12:43:13 +0900

    add Dropdown component

---
+++ components/ui/Dropdown.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import cn from 'classnames';
+
+interface DropdownItem {
+  icon?: React.ReactNode;
+  label: string;
+  onClick: () => void;
+}
+
+interface Props {
+  button: React.ReactNode;
+  dropdownItems: DropdownItem[];
+}
+
+const Dropdown: React.FC<Props> = ({ button, dropdownItems }) => {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      {({ open }) => (
+        <>
+          <div>
+            <Menu.Button className="rounded-md">{button}</Menu.Button>
+          </div>
+          <Transition
+            show={open}
+            as={React.Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+            >
+              {dropdownItems.map(({ icon, label, onClick }, idx) => (
+                <Menu.Item key={`dropdownItem-${idx}-${label}`}>
+                  {({ active }) => (
+                    <button
+                      className={cn(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'w-full flex items-center px-4 py-2 text-sm',
+                      )}
+                      onClick={onClick}
+                    >
+                      {icon}
+                      <span className={cn({ 'ml-3': icon !== undefined })}>
+                        {label}
+                      </span>
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
+  );
+};
+
+export default Dropdown;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import NextImage from 'next/image';
+import Dropdown from '@components/ui/Dropdown';
+
+import { ChevronDownIcon } from '@heroicons/react/solid';
 
 const IndexPage = () => {
   return (
     <div className="mx-auto max-w-screen-lg text-2xl pt-4">
-      <p className="text-xl">hello world</p>
-      <NextImage
-        src="/images/preparing.png"
-        layout="intrinsic"
-        width={500}
-        height={463}
-      />
+      {/* <p className="text-xl">hello world</p> */}
+      <div>
+        <Dropdown
+          button={
+            <div className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
+              options
+              <ChevronDownIcon
+                className="-mr-1 ml-2 h-5 w-5"
+                aria-hidden="true"
+              />
+            </div>
+          }
+          dropdownItems={[
+            { label: 'hello', onClick: () => {} },
+            { label: 'world', onClick: () => {} },
+          ]}
+        />
+      </div>
     </div>
   );
 };
