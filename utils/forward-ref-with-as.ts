bd1c5d20ece6893178a086a71a955eae7d76e2d8
Kay Kim <kimjh@bawi.org>
2021-07-05 16:23:02 +0900

    minor fix

--- utils/forward-ref-with-as.ts
+++ utils/forward-ref-with-as.ts
@@ -7,7 +7,7 @@ import { forwardRef } from 'react';
 export function forwardRefWithAs<T extends { name: string; displayName?: string }>(
   component: T,
 ): T & { displayName: string } {
-  return Object.assign(forwardRef(component as unknown as any) as any, {
+  return Object.assign(forwardRef(component as any) as any, {
     displayName: component.displayName ?? component.name,
   });
 }
