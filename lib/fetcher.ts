212c598c66744be9e04bbb689300342ff98f86ac
Kay Kim <kimjh@bawi.org>
2021-07-07 13:38:50 +0900

    v3.2.3
    add exception for not-JSON type response body

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,14 @@
 export async function fetcher<T = any>(url: string, init?: RequestInit): Promise<T> {
   const response = await fetch(url, init);
 
+  if (!response.ok) {
+    throw await response.json();
+  }
+
+  const contentType = response.headers.get('Content-Type');
+  if (!contentType || contentType.indexOf('application/json') === -1) {
+    return (await response.text()) as never;
+  }
+
   return await response.json();
 }
