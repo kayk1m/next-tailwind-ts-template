dc413c5cad2b87df6583aecc3038f45947f1ddfc
Kay Kim <kimjh@bawi.org>
2021-07-15 18:29:00 +0900

    remove strict checking from is-string validator

--- utils/validator/common/is-string.ts
+++ utils/validator/common/is-string.ts
@@ -1,54 +1,19 @@
-import { createError } from '@defines/errors';
-
 export default function isString(
   val: any,
-  opts?: { strict?: boolean; minLength?: number; maxLength?: number },
+  opts?: { minLength?: number; maxLength?: number },
 ): val is string {
   const minLength = opts?.minLength ? opts.minLength : null;
   const maxLength = opts?.maxLength ? opts.maxLength : null;
 
   if (typeof val !== 'string') {
-    if (opts?.strict) {
-      throw createError('VALIDATION_FAILED', {
-        name: 'Type error',
-        message: JSON.stringify({
-          val,
-          expected: 'string',
-          received: typeof val,
-        }),
-      });
-    }
-
     return false;
   }
 
   if (minLength && val.length < minLength) {
-    if (opts?.strict) {
-      throw createError('VALIDATION_FAILED', {
-        name: 'Unallowed string length',
-        message: JSON.stringify({
-          val,
-          minLength,
-          received: val.length,
-        }),
-      });
-    }
-
     return false;
   }
 
   if (maxLength && val.length > maxLength) {
-    if (opts?.strict) {
-      throw createError('VALIDATION_FAILED', {
-        name: 'Unallowed string length',
-        message: JSON.stringify({
-          val,
-          maxLength,
-          received: val.length,
-        }),
-      });
-    }
-
     return false;
   }
 
