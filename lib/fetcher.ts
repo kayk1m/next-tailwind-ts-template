59dd2ef231fddcdf3c29502e5c2d8201aba3eb86
kjsik11 <kjsik11@gmail.com>
2021-08-22 12:53:39 +0900

    fix fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,31 +1,29 @@
 import ky from 'ky';
 
-// export async function fetcher<T = any>(url: string, init?: RequestInit): Promise<T> {
-//   const response = await fetch(url, init);
+export async function fetcher<T = any>(url: string, init?: RequestInit): Promise<T> {
+  const extKy =  ky.extend({
+    hooks: {
+      afterResponse: [
+        async (_req, _opt, res) => {
+          if (!res.ok) {
+              const contentType = res.headers.get('Content-Type');
+              if (!contentType || contentType.indexOf('application/json') === -1) 
+                throw await res.text();
+              throw await res.json();
+          }
+        },
+      ],
+    },
+  });
+  
+  const response = await extKy(url,init)
 
-//   if (!response.ok) {
-//     const contentType = response.headers.get('Content-Type');
-//     if (!contentType || contentType.indexOf('application/json') === -1) {
-//       throw await response.text();
-//     }
+  const contentType = response.headers.get('Content-Type');
+  if (!contentType || contentType.indexOf('application/json') === -1) {
+    return (await response.text()) as never;
+  }
 
-//     throw await response.json();
-//   }
+  return await response.json();
+}
 
-//   const contentType = response.headers.get('Content-Type');
-//   if (!contentType || contentType.indexOf('application/json') === -1) {
-//     return (await response.text()) as never;
-//   }
 
-//   return await response.json();
-// }
-
-export const fetcher = ky.extend({
-  hooks: {
-    afterResponse: [
-      async (_req, _opt, res) => {
-        if (!res.ok) throw await res.json();
-      },
-    ],
-  },
-});
