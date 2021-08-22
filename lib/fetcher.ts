e3738c8ce5c37a2014ea04c4be653151583b7019
kjsik11 <kjsik11@gmail.com>
2021-08-22 13:24:36 +0900

    d

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -4,10 +4,13 @@ export const fetcher = ky.extend({
   hooks: {
     afterResponse: [
       async (_req, _opt, res) => {
-        const contentType = res.headers.get('Content-Type');
-        if (!contentType || contentType.indexOf('application/json') === -1) 
-          throw await res.text();
-        if (!res.ok) throw await res.json();
+    
+        if (!res.ok) {
+          const contentType = res.headers.get('Content-Type');
+          if (!contentType || contentType.indexOf('application/json') === -1) 
+            throw await res.text();
+          throw await res.json();
+        }
       },
     ],
   },
