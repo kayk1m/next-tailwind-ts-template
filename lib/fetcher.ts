170636c766fcd78c452d15a416c84dca38f9c196
kjsik11 <kjsik11@gmail.com>
2021-08-22 13:27:55 +0900

    remove swr fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -4,7 +4,6 @@ export const fetcher = ky.extend({
   hooks: {
     afterResponse: [
       async (_req, _opt, res) => {
-    
         if (!res.ok) {
           const contentType = res.headers.get('Content-Type');
           if (!contentType || contentType.indexOf('application/json') === -1) 
@@ -15,7 +14,3 @@ export const fetcher = ky.extend({
     ],
   },
 });
-
-// export async function swrFetcher<T = any>(url: string, init?: RequestInit): Promise<T> {
-//   return await fetcher(url,init).json()
-// }
