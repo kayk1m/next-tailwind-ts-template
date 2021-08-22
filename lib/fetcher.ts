a69c2e854a670b45a7249dda36ce40a5881f4280
kjsik11 <kjsik11@gmail.com>
2021-08-22 13:11:01 +0900

    .

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,29 +1,19 @@
 import ky from 'ky';
 
-export async function fetcher<T = any>(url: string, init?: RequestInit): Promise<T> {
-  const extKy =  ky.extend({
-    hooks: {
-      afterResponse: [
-        async (_req, _opt, res) => {
-          if (!res.ok) {
-              const contentType = res.headers.get('Content-Type');
-              if (!contentType || contentType.indexOf('application/json') === -1) 
-                throw await res.text();
-              throw await res.json();
-          }
-        },
-      ],
-    },
-  });
-  
-  const response = await extKy(url,init)
+export const fetcher = ky.extend({
+  hooks: {
+    afterResponse: [
+      async (_req, _opt, res) => {
+        const contentType = res.headers.get('Content-Type');
+        if (!contentType || contentType.indexOf('application/json') === -1) 
+          throw await res.text();
+        if (!res.ok) throw await res.json();
+      },
+    ],
+  },
+});
 
-  const contentType = response.headers.get('Content-Type');
-  if (!contentType || contentType.indexOf('application/json') === -1) {
-    return (await response.text()) as never;
-  }
-
-  return await response.json();
+export async function swrFetcher<T = any>(url: string, init?: RequestInit): Promise<T> {
+  return await fetcher(url,init).json()
+ 
 }
-
-
